d2179a3c99a83bec9aeebe6dca78f42c1d3d1898
A-Capelini <anderson_capelini@yahoo.com.br>
2022-05-02 18:42:33 -0300

    Realizado o exercicio referente ao encontro on line 2

---
+++ UC8-lógica-de-programção/exercicioAula_2.js
@@ -0,0 +1,9 @@
+//registrar 3 nomes
+//Array com 4 posições
+//              0          1           2         3
+let nomes = ["Carlos", "Ronaldo", "Odirley", "Cinthia"];
+
+for(let contador = 0; contador <=3; contador ++)
+{
+    console.log(nomes[contador]);
+}
